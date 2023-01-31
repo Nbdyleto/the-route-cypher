b869b599493876bf94dfe4e7b4ff0ff03389cbcc
Nbdyleto <nobodyleto2004@gmail.com>
2023-01-31 05:30:08 -0300

    creating base logic, w/ original message and encryption matrix

--- decryption_code.py
+++ decryption_code.py
@@ -1,1 +1,16 @@
+original_msg = "Hey I Like Cook French Potatos and I Really Believe That I Will Be A Great Chef Kitchen One Day"
+key = [-1, 2, -3, 4] # "Negative numbers mean you start at the bottom and read up a column; positive numbers mean the reverse."
 
+spl_msg = original_msg.split()
+encryption_matrix = [] 
+count = 0
+for row in range(5):
+    temp_matrix = []
+    for col in range(4):
+        word = spl_msg[count]
+        temp_matrix.append((word, count))
+        count += 1
+    encryption_matrix.append(temp_matrix)
+print(encryption_matrix)  
+
+# Now, get the cyphertext, in the order they where read.
